69d4c1a3e617513d68ce0121c2c3d28f006e8f37
AkiLapek <ontur00@gmail.com>
2016-02-20 07:46:06 +0100

    a
    change info padding , o co chodzi href

--- index.php
+++ index.php
@@ -33,7 +33,7 @@
         <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
             <ul class="nav navbar-nav navbar-right">
                 <li>
-                    <a href="?page=home">O co chodzi</a>
+                    <a href="?page=info">O co chodzi</a>
                 </li>
             </ul>
             <ul class="nav navbar-nav navbar-right">
