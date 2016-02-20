7245ab2d63deba8b6bf5aa8511239665f89a64f5
AkiLapek <ontur00@gmail.com>
2016-02-20 15:51:08 +0100

    add layout registerAdvertisment

--- registerAdvertisment.php
+++ registerAdvertisment.php
@@ -11,93 +11,72 @@
         <div class="form-group">
             <label for="website" class="col-sm-3 control-label">Adres strony</label>
             <div class="col-sm-9">
-                <input type="text" id="website" placeholder="Full Name" class="form-control" autofocus>
-                <span class="help-block">Last Name, First Name, eg.: Smith, Harry</span>
+                <input type="text" id="website" placeholder="" class="form-control" autofocus>
+                <span class="help-block">Podaj adres strony ze ogłoszeniem</span>
             </div>
         </div>
         <div class="form-group">
-            <label for="email" class="col-sm-3 control-label">Email</label>
+            <label for="position" class="col-sm-3 control-label">Stanowisko</label>
             <div class="col-sm-9">
-                <input type="email" id="email" placeholder="Email" class="form-control">
+                <input type="catedory" id="category" placeholder="" class="form-control">
             </div>
         </div>
         <div class="form-group">
-            <label for="password" class="col-sm-3 control-label">Password</label>
+            <label for="firm" class="col-sm-3 control-label">Firma</label>
             <div class="col-sm-9">
-                <input type="password" id="password" placeholder="Password" class="form-control">
+                <input type="password" id="password" placeholder="" class="form-control">
             </div>
         </div>
         <div class="form-group">
-            <label for="birthDate" class="col-sm-3 control-label">Date of Birth</label>
-            <div class="col-sm-9">
-                <input type="date" id="birthDate" class="form-control">
-            </div>
-        </div>
-        <div class="form-group">
-            <label for="country" class="col-sm-3 control-label">Country</label>
+            <label for="province" class="col-sm-3 control-label">Województwo</label>
             <div class="col-sm-9">
                 <select id="country" class="form-control">
-                    <option>Afghanistan</option>
-                    <option>Bahamas</option>
-                    <option>Cambodia</option>
-                    <option>Denmark</option>
-                    <option>Ecuador</option>
-                    <option>Fiji</option>
-                    <option>Gabon</option>
-                    <option>Haiti</option>
+                    <option>Dolnośląskie</option>
+                    <option>Kujawsko-pomorskie</option>
+                    <option>Lubuskie</option>
+                    <option>Łódzkie</option>
+                    <option>Małopolskie</option>
+                    <option>Mazowieckie</option>
+                    <option>Opolskie</option>
+                    <option>Podkarpackie</option>
+                    <option>Podlaskie</option>
+                    <option>Pomorskie</option>
+                    <option>Śląskie</option>
+                    <option>Świętokrzyskie</option>
+                    <option>Warmińsko-mazurskie</option>
+                    <option>Wielkopolskie</option>
+                    <option>Opolskie</option>
+                    <option>Zachodniopomorskie</option>
                 </select>
             </div>
         </div> <!-- /.form-group -->
+
         <div class="form-group">
-            <label class="control-label col-sm-3">Gender</label>
-            <div class="col-sm-6">
-                <div class="row">
-                    <div class="col-sm-4">
-                        <label class="radio-inline">
-                            <input type="radio" id="femaleRadio" value="Female">Female
-                        </label>
-                    </div>
-                    <div class="col-sm-4">
-                        <label class="radio-inline">
-                            <input type="radio" id="maleRadio" value="Male">Male
-                        </label>
-                    </div>
-                    <div class="col-sm-4">
-                        <label class="radio-inline">
-                            <input type="radio" id="uncknownRadio" value="Unknown">Unknown
-                        </label>
-                    </div>
-                </div>
-            </div>
-        </div> <!-- /.form-group -->
-        <div class="form-group">
-            <label class="control-label col-sm-3">Meal Preference</label>
+            <label for="city" class="col-sm-3 control-label">Miasto</label>
             <div class="col-sm-9">
-                <div class="checkbox">
-                    <label>
-                        <input type="checkbox" id="calorieCheckbox" value="Low calorie">Low calorie
-                    </label>
-                </div>
-                <div class="checkbox">
-                    <label>
-                        <input type="checkbox" id="saltCheckbox" value="Low salt">Low salt
-                    </label>
-                </div>
+                <input type="text" id="text" placeholder="" class="form-control">
+            </div>
+        </div> <!-- /.form-group -->
+
+        <div class="form-group">
+            <label for="city" class="col-sm-3 control-label">Zamieszczono dnia</label>
+            <div class="col-sm-9">
+                <input type="text" id="text" placeholder="" class="form-control">
+            </div>
+        </div> <!-- /.form-group -->
+
+        <div class="form-group">
+            <label for="city" class="col-sm-3 control-label">Oferta wygasa dnia</label>
+            <div class="col-sm-9">
+                <input type="text" id="text" placeholder="" class="form-control">
             </div>
         </div> <!-- /.form-group -->
         <div class="form-group">
             <div class="col-sm-9 col-sm-offset-3">
-                <div class="checkbox">
-                    <label>
-                        <input type="checkbox">I accept <a href="#">terms</a>
-                    </label>
-                </div>
-            </div>
-        </div> <!-- /.form-group -->
-        <div class="form-group">
-            <div class="col-sm-9 col-sm-offset-3">
-                <button type="submit" class="btn btn-primary btn-block">Register</button>
+                <button type="submit" class="btn btn-primary btn-block">Rejestracja</button>
             </div>
         </div>
+
+
     </form> <!-- /form -->
 </div> <!-- ./container -->
