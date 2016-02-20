ea7b480b74b72db6040e309c1e6a8e09bf0793a6
AkiLapek <ontur00@gmail.com>
2016-02-20 05:08:18 +0100

    chage main page
    change layout main page

--- home.php
+++ home.php
@@ -1,28 +1,46 @@
 <!-- Header -->
 <div class="intro-header">
+
     <div class="container">
         <div class="row">
             <div class="col-lg-12">
                 <div class="intro-message">
                     <h1>Przez 'Czystszą' do Pracy</h1>
-                    <h3>A Template by Start Bootstrap</h3>
                     <hr class="intro-divider">
                     <ul class="list-inline intro-social-buttons">
                         <li>
-                            <a href="?page=find_job" class="btn btn-default btn-lg"><i class="fa fa-twitter fa-fw"></i> <span class="network-name">Twitter</span></a>
+                            <a href="?page=find_job" class="btn btn-default btn-lg"><i class=></i> <span class="network-name">Człowiek szuka pracy</span></a>
                         </li>
                         <li>
-                            <a href="https://github.com/IronSummitMedia/startbootstrap" class="btn btn-default btn-lg"><i class="fa fa-github fa-fw"></i> <span class="network-name">Github</span></a>
-                        </li>
-                        <li>
-                            <a href="#" class="btn btn-default btn-lg"><i class="fa fa-linkedin fa-fw"></i> <span class="network-name">Linkedin</span></a>
+                            <a href="" class="btn btn-default btn-lg"><i class=></i> <span class="network-name">Praca szuka człowieka</span></a>
                         </li>
                     </ul>
                 </div>
             </div>
         </div>
+        <!-- Login tabbed -->
+        <nav class="">
+            <div class="container-fluid">
+                <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
+                    <form id="signin" class="navbar-form navbar-right" role="form">
+                        <div class="input-group">
+                            <span class="input-group-addon"><i class="glyphicon glyphicon-user"></i></span>
+                            <input id="email" type="email" class="form-control" name="email" value="" placeholder="Email Address">
+                        </div>
 
+                        <div class="input-group">
+                            <span class="input-group-addon"><i class="glyphicon glyphicon-lock"></i></span>
+                            <input id="password" type="password" class="form-control" name="password" value="" placeholder="Password">
+                        </div>
+
+                        <button type="submit" class="btn btn-primary">Login</button>
+                    </form>
+
+                </div>
+            </div>
+        </nav>
     </div>
     <!-- /.container -->
 </div>
+
 <!-- /.intro-header -->
