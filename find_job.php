92644be29bc066af2b87dc985a0af53bbffe68dd
Levvy055 <mizyn24@gmail.com>
2016-02-20 09:52:16 +0100

    simple table row generator

--- find_job.php
+++ find_job.php
@@ -17,14 +17,16 @@ $regex = '~(http(s?)://)([^/]*)/?~';
         <div class="row">
             <div class="panel panel-primary filterable">
                 <div class="panel-heading">
-                    <h3 class="panel-title">Users</h3>
+                    <h3 class="panel-title">Ogłoszenia</h3>
                     <div class="pull-right">
-                        <button class="btn btn-default btn-xs btn-filter"><span class="glyphicon glyphicon-filter"></span> Filtruj</button>
+                        <button class="btn btn-default btn-xs btn-filter">
+                            <span class="glyphicon glyphicon-filter"></span> Filtruj
+                        </button>
                     </div>
                 </div>
-                <table id="jobs_table" class="table">
+                <table class="table display dataTable dtr-inline collapsed bordered table-striped">
                     <thead>
-                    <tr class="filters">
+                    <tr class="filters" style="font-weight: bolder;">
                         <th><input type="text" class="form-control" placeholder="Strona" disabled></th>
                         <th><input type="text" class="form-control" placeholder="Kategoria" disabled></th>
                         <th><input type="text" class="form-control" placeholder="Województwo" disabled></th>
@@ -38,28 +40,22 @@ $regex = '~(http(s?)://)([^/]*)/?~';
                     <tbody>
                     <?php
                     foreach ($adverts as $advK => $adv) {
-                        $link = '<a href="'.$adv[1].'">';
+                        $link = '<a href="' . $adv[1] . '">';
                         $end = '</a>';
                         preg_match($regex, $adv[1], $matches);
                         $site = $matches[0];
-                        echo "<tr><td>".$link.$site.$end."</td><td>".$link.$adv[2].$end."</td><td>".$link.$adv[3].$end."</td><td>".$link.$adv[4].$end."</td><td>";
-                        echo $link.$adv[5].$end."</td><td>".$link.$adv[6].$end."</td><td>".$link.$adv[7].$end."</td><td>";
-                        echo $link.$adv[8].$end."</td><td>";
-                        echo "</td></tr>";
-                    }; ?>
+                        echo "<tr><td>" . $link . $site . $end . "</td>";
+                        for ($i = 2; $i < 9; $i++) {
+                            echo "<td>" . $link . $adv[$i] . $end . "</td>";
+                        }
+                        echo "</tr>";
+                    }
+                    ?>
                     </tbody>
                 </table>
-                <script>
-                    $('tr[data-href]').on("click", function() {
-                        document.location = $(this).data('href');
-                    });
-                </script>
+                <script src="js/vendor/jquery-1.12.0.min.js"></script>
+                <script type="text/javascript" src="/js/vendor/jquery.tablesorter.js"></script>
                 <script src="https://cdn.datatables.net/1.10.11/js/jquery.dataTables.min.js"></script>
-                <script>
-                    $(document).ready(function(){
-                        $('#jobs_table').DataTable();
-                    });
-                </script>
             </div>
         </div>
     </div>
