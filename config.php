5a2f9027b2223ac63550a9f328fbc10579d1ef58
Levvy055 <mizyn24@gmail.com>
2016-02-20 02:50:25 +0100

    config

---
+++ config.php
@@ -0,0 +1,14 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Lukasz
+ * Date: 2/20/2016
+ * Time: 2:13 AM
+ */
+define('DATABASE_NAME', '');
+
+define('DATABASE_USER', '');
+
+define('DATABASE_PASS', '');
+
+define('DATABASE_HOST', '');
